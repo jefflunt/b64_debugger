e825cd0c70542c79da3ccc0491ce853b0bf76a95
jefflunt <jefflunt@gmail.com>
2019-09-28 12:41:09 -0500

    UTF-8 support

--- b64_debugger.rb
+++ b64_debugger.rb
@@ -5,7 +5,7 @@ CONVERTER = [
   '+', '/', '='
 ].flatten
 
-def abs_format(bit_str)
+def ubs_format(bit_str)
   bit_str
     .scan(/.{1,8}/m)
     .join(' ')
@@ -17,10 +17,20 @@ def bbs_format(bit_str)
     .join(' ')
 end
 
-def ascii_to_bit_str(ascii)
-  ascii
-    .chars
-    .map{|c| c.ord.to_s(2).rjust(8, '0') }
+def char_to_bits(c)
+  ord_bits = c
+    .ord
+    .to_s(2)
+
+  ord_length = (ord_bits.length / 8.0).ceil * 8
+  ord_bits
+    .rjust(ord_length, '0')
+end
+
+def utf8_to_bit_str(utf8)
+  utf8
+    .bytes
+    .map{|b| b.to_s(2).rjust(8, '0') }
     .join
 end
 
@@ -43,23 +53,24 @@ def b64_to_bit_str(b64)
     .join
 end
 
-def bit_str_to_ascii(bit_str)
+def bit_str_to_utf8(bit_str)
   bit_str
     .scan(/.{1,8}/m)
-    .map{|bits| bits.to_i(2).chr }
-    .join
+    .map{|byte_str| byte_str.to_i(2) }
+    .pack('c*')
+    .force_encoding('UTF-8')
 end
 
-ascii_in = ARGV.join(' ')
-bit_str8 = ascii_to_bit_str(ascii_in)
+utf8_in = gets.encode('UTF-8').chomp
+bit_str8 = utf8_to_bit_str(utf8_in)
 b64 = bit_str_to_b64(bit_str8)
 bit_str6 = b64_to_bit_str(b64)
-ascii_out = bit_str_to_ascii(bit_str6)
+utf8_out = bit_str_to_utf8(bit_str6)
 
-puts "ASC: #{ascii_in}"
-puts "A>8: #{abs_format(bit_str8)}"
-puts "8>6: #{bbs_format(bit_str8)}"
-puts "6>B: #{b64}"
-puts "B>6: #{bbs_format(bit_str6)}"
-puts "6>8: #{abs_format(bit_str6)}"
-puts "8>A: #{ascii_out}"
+puts "->        UTF-8: #{utf8_in}"
+puts "-> 8-bit binary: #{ubs_format(bit_str8)}"
+puts "-> 6-bit binary: #{bbs_format(bit_str8)}"
+puts "->       Base64: #{b64}"
+puts "-> 6-bit binary: #{bbs_format(bit_str6)}"
+puts "-> 8-bit binary: #{ubs_format(bit_str6)}"
+puts "->        UTF-8: #{utf8_out}"
